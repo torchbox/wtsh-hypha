torchbox/wtsh-hypha

commit 6fe6531c9f593c8f5197a7f515d3d0da06490de3

Add workaround for bug in application-form.js

hypha/static_src/javascript/application-form.js
@@ -51,7 +51,9 @@
 
   // Block multiple form submits.
   form.addEventListener("submit", function () {
-    button.setAttribute("disabled", "disabled");
+    window.setTimeOut(function () {
+      button.setAttribute("disabled", "disabled");
+    });
   });
 
   const unlockApplicationForm = function () {

commit 94fd1aa7d0227d297c06803359742d7425c045f1

Fix typo in setTimeout function name
Introduced in 6fe6531c9f593c8f5197a7f515d3d0da06490de3

hypha/static_src/javascript/application-form.js
@@ -51,7 +51,7 @@
 
   // Block multiple form submits.
   form.addEventListener("submit", function () {
-    window.setTimeOut(function () {
+    window.setTimeout(function () {
       button.setAttribute("disabled", "disabled");
     });
   });